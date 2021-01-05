afc5edc4c68de3fe81b619b995683b3c7586df82
Mohamed Saber <mohamedsaber19977@gmail.com>
2021-01-05 05:58:19 +0200

    some code refactor

--- FinalProject.m
+++ FinalProject.m
@@ -8,16 +8,15 @@ clc;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Part 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Reading from audio file
 [signal,Fs] = audioread('eric.wav');
-
 %%%%%%%%%%%%%%%%  Time Domain  %%%%%%%%%%%%%%%%
 %Number of samples
 samplesNumber = length(signal);
 %Distribute of points on the x axis
-t= linspace(0,samplesNumber/Fs,samplesNumber);
+time= linspace(0,samplesNumber/Fs,samplesNumber);
 %show figure 1
 figure ('Name','DOUBLE SIDEBAND MODULATION','NumberTitle','off');
 subplot(2,1,1);
-plot(t,signal);
+plot(time,signal);
 title('Original Signal in Time Domain');
 ylabel('Amplitude');
 xlabel('Time');
@@ -42,3 +41,18 @@ cutoffFrequency=4000/(Fs/2);
 [denumerator,numerator]=butter(20,cutoffFrequency,'low');
 filteredSignalTime=filter(denumerator,numerator,signal);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Part 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Frequency domain
+filteredSignalFrequency=fftshift(fft(filteredSignalTime));
+%Show figure 2
+figure ('Name','DOUBLE SIDEBAND MODULATION','NumberTitle','off');
+subplot(2,1,1);
+plot(time,filteredSignalTime);
+title('Signal filtered by Low pass filter in Time Domain');
+ylabel('Amplitude');
+xlabel('Time');
+subplot(2,1,2);
+plot(frequencyRange,abs(filteredSignalFrequency));
+title('Signal filtered by Low pass filter in Frequency Domain');
+ylabel('Amplitude');
+xlabel('Frequency');
